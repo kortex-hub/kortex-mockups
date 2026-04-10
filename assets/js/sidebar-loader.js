64c846ffb06f233b746f2d1364077fc96e128e79
Stevan Le Meur <1636769+slemeur@users.noreply.github.com>
2026-04-10 09:55:23 +0200

    minor changes

--- assets/js/sidebar-loader.js
+++ assets/js/sidebar-loader.js
@@ -17,13 +17,6 @@ function buildSidebarHTML() {
     const R = getNavRoot();
     return `
 <nav class="sidebar">
-    <a href="${R}/chat/index.html" class="nav-item" data-section="chat">
-        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
-        </svg>
-        <span>Chat</span>
-    </a>
-
     <a href="${R}/sessions/index.html" class="nav-item" data-section="sessions">
         <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <path d="M12 2a2 2 0 0 1 2 2c0 .74-.4 1.39-1 1.73V7h1a7 7 0 0 1 7 7h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v1a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-1H2a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h1a7 7 0 0 1 7-7h1V5.73c-.6-.34-1-.99-1-1.73a2 2 0 0 1 2-2z"/>
